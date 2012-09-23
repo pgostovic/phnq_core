e75e7607143d98f24288c272339cf8113ba95517
Patrick Gostovic <pg@phranq.com>
2012-09-23 10:51:59 -0400

    Added some unit tests.

---
+++ test/phnq_core_test.js
@@ -0,0 +1,39 @@
+var assert = require("assert");
+var phnq_core = require("phnq_core");
+
+describe("phnq_core", function()
+{
+	describe("extend()", function()
+	{
+		it("should extend the destination object with the source object's keys and values", function()
+		{
+			var o1 = {foo:"bar"};
+			var o2 = {num:42};
+			phnq_core.extend(o1, o2);
+			assert.deepEqual(o1, {foo:"bar", num:42});
+		});
+	});
+
+	describe("escapeJS()", function()
+	{
+		it("should escape quotes", function()
+		{
+			assert.equal(phnq_core.escapeJS("he said \"hello\"."), "he said \\\"hello\\\".");
+		});
+
+		it("should escape newlines", function()
+		{
+			assert.equal(phnq_core.escapeJS("line 1\nline 2\nline 3"), "line 1\\nline 2\\nline 3");
+		});
+	});
+
+	describe("trimLines()", function()
+	{
+		it("should trim all the lines", function()
+		{
+			var untrimmed = "  \t   \t  ONE   \n      TWO    \n    THREE     ";
+			var trimmed = "ONE\nTWO\nTHREE";
+			assert.equal(phnq_core.trimLines(untrimmed), trimmed);
+		});
+	});
+});
