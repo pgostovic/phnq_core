19e0363d5ae8340672f25580b624b4760fa70073
Patrick Gostovic <pg@phranq.com>
2013-05-28 17:11:36 -0400

    Added rmdir()

--- phnq_core.js
+++ phnq_core.js
@@ -132,7 +132,37 @@
 			this.assertServer();
 			return __filename;
 		},
-
+		
+		rmdir: function(dir)
+		{
+			this.assertServer();
+			
+			var fs = require("fs");
+			var path = require("path");
+			
+			var list = fs.readdirSync(dir);
+			for(var i = 0; i < list.length; i++)
+			{
+				var filename = path.join(dir, list[i]);
+				var stat = fs.statSync(filename);
+		
+				if(filename == "." || filename == "..")
+				{
+					// pass these files
+				}
+				else if(stat.isDirectory())
+				{
+					// rmdir recursively
+					this.rmdir(filename);
+				}
+				else
+				{
+					// rm fiilename
+					fs.unlinkSync(filename);
+				}
+			}
+			fs.rmdirSync(dir);
+		}
 
 		/*
 			Call like phnq_core.parallel(fn1, fn2, ... , fnN, callback). Example...
